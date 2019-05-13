71b2a263886f90b073977524a7cec5e634df2c32
Jan Ramdohr <jramdohr23@gmail.com>
2019-05-13 09:06:50 -0500

    updated python exercises

--- Practice Python/#20 Element Search.py
+++ Practice Python/#20 Element Search.py
@@ -1,7 +1,21 @@
 #Exercise 20 (and Solution)
-
+'''
 Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
 
 Extras:
 
-Use binary search.
+Use binary search.'''
+
+import random
+
+list = [i for i in range(1,10)]
+number = random.randint(1,20)
+
+def isInList (list, number):
+    if number in list:
+        return True
+    else:
+        return False
+        
+print(isInList(list, number))
+print('because %s was the number. The list has the values 1-9.' %(number))
--- Project Euler/#5 Smallest Multiple.py
+++ Project Euler/#5 Smallest Multiple.py
@@ -1,4 +1,27 @@
 # Problem 5
+'''
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
+
+i = 1
+j = 1
+
+numbers = [2,3,4,5,6,7,8,9,10]
+
+while i:
+    if i%2 == 0 and i%3 == 0 and i%4 == 0 and i%5 == 0 and i%6 == 0 and i%7 == 0 and i%8 == 0 and i%9 == 0 and i%10 == 0:
+        print("It's " + str(i))
+        break
+    i += 1
+  
+
+#if i%range(1,20):
+    
+while j:
+    if j%2 == 0 and j%3 == 0 and j%4 == 0 and j%5 == 0 and i%6 == 0 and j%7 == 0 and i%8 == 0 and j%9 == 0 and i%10 == 0 and j%11 == 0 and i%12 == 0 and j%13 == 0 and i%14 == 0 and i%15 == 0 and j%16 == 0 and j%17 == 0 and i%18 == 0 and j%19 == 0 and j%20 == 0:
+        print("It's " + str(j))
+        break
+    j += 1
