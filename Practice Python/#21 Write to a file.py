a4b5dab307e401d02ab5fe205daf696a167ae6e9
Jan Ramdohr <jramdohr23@gmail.com>
2019-06-02 11:50:29 -0500

    Updated #21

--- Practice Python/#21 Write to a file.py
+++ Practice Python/#21 Write to a file.py
@@ -6,4 +6,44 @@ Extras:
 
 Ask the user to specify the name of the output file that will be saved.'''
 
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
 
+date = date.today()
+
+print('New York Times Headlines - File Export')
+print('*'*40)
+
+#Bonus
+getFileName = input('Do you want to name the file? y/n')
+while True:
+    if getFileName == 'y':
+        custom_name = input('Please enter file name: ')
+        file = f'{custom_name}.txt'
+        break
+    elif getFileName == 'n':
+        file = f'headlines {date}.txt'
+        break
+    else:
+        print('Please reply with (y)es or (n)o.')
+        getFileName = input('Do you want to name the file? y/n')
+#
+
+print('\nfetching your headlines...')
+
+url = 'http://www.nytimes.com'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+
+h2_tags = soup.findAll('h2')
+
+with open(file, 'w') as fileobject:
+    fileobject.write("Today's Headlines:\n")
+    for line in h2_tags:
+        fileobject.write(line.text.encode('ascii', 'ignore').decode('ascii') + '\n')
+    fileobject.
+    
+
+print(f'Operation complete. File {file} created')
